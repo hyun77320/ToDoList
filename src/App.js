f63c398072c9abfdb67eba1cc68201ff243e2153
iqamac <hyun77320@naver.com>
2022-04-12 22:53:33 +0900

    2022.04.12.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, {useState, useRef, useCallback} from 'react';
 import TodoTemplate from './component/TodoTemplate';
 import TodoInsert from './component/TodoInsert';
+import TodoList from './component/TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id:1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해 보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+          id: nextId.current,
+        text,
+          checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
   return (
     <div>
       <TodoTemplate>
-        <TodoInsert></TodoInsert>
+        <TodoInsert onInsert={onInsert}/>
+        <TodoList todos={todos}/>
       </TodoTemplate>
     </div>
   );
