1190476af24d404243027203b28286f52d1edd23
iqamac <hyun77320@naver.com>
2022-04-25 22:29:39 +0900

    2022.04.25.

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -57,8 +57,9 @@ function TodoHead() {
 
     return (
         <TodoHeadBlock>
-            <h1>{year + "년" +  month + "월" + date  + "일"}</h1>
+            <h1>{year + "년 " +  month + "월 " + date  + "일"}</h1>
             <div className='day'>{state.day}요일</div>
+            <div className='tasks-left'>할 일 2개 남음</div>
         </TodoHeadBlock>
     );
 }
